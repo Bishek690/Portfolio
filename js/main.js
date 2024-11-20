587a4e03ee9696ba94862ff76b21c5896df4ca78
Bishek Chaudhary <bishekchaudhary2000@gmail.com>
2024-11-20 13:09:44 +0545

    Initial commit: Uploading project

---
+++ js/main.js
@@ -0,0 +1,54 @@
+
+
+// Header Scroll
+
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 20) {
+        nav.classList.add("header-scrolled");
+    } else {
+        nav.classList.remove("header-scrolled");
+    }
+}
+
+// Navbar Hide
+
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a) {
+    a.addEventListener("click", function () {
+        navCollapse.classList.remove("show");
+    })
+})
+
+// EmailJS
+
+  emailjs.init("TcVCPEa3CupXLV2BY"); 
+
+  function sendEmail(event) {
+    event.preventDefault(); 
+
+    const form = event.target; 
+
+    const templateParams = {
+      from_name: form.querySelector('input[placeholder="Your Name"]').value,
+      from_email: form.querySelector('input[placeholder="Your Email"]').value,
+      phone: form.querySelector('input[placeholder="Your Phone Number"]').value,
+      message: form.querySelector('textarea[placeholder="Your Message"]').value,
+    };
+
+    emailjs
+      .send("service_awpac52", "template_2c1e3ap", templateParams)
+      .then(
+        function (response) {
+          alert("Email sent successfully!");
+          form.reset(); // Reset the form fields
+        },
+        function (error) {
+          alert("Failed to send email. Please try again later.");
+        }
+      );
+  }
+
+  // Attach the event listener to the form
+  document.querySelector("form").addEventListener("submit", sendEmail);
